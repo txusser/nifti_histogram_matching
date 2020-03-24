b96ed4d678e93a620d79f329d3f0dbdb6e8fb540
Jesus Silva <jesus@qubiotech.com>
2020-03-24 11:09:02 +0100

    added first files

---
+++ nifti_histogram_matching.py
@@ -0,0 +1,208 @@
+import nibabel as nib
+import numpy as np
+
+def histogram_matching(reference_nii, input_nii, output_nii):
+
+    # Load the template image
+    template = nib.load(reference_nii)
+    nt_data = template.get_data()[:,:,:]
+    
+    # Load the patient image
+    patient = nib.load(input_nii)
+    pt_data = patient.get_data()[:,:,:]
+
+    # Stores the image data shape that will be used later
+    oldshape = pt_data.shape
+
+    # Converts the data arrays to single dimension and normalizes by the maximum
+    nt_data_array = nt_data.ravel()
+    pt_data_array = pt_data.ravel()
+
+    # get the set of unique pixel values and their corresponding indices and counts
+    s_values, bin_idx, s_counts = np.unique(pt_data_array, return_inverse=True, return_counts=True)
+    t_values, t_counts = np.unique(nt_data_array, return_counts=True)
+
+    # take the cumsum of the counts and normalize by the number of pixels to
+    # get the empirical cumulative distribution functions for the source and
+    # template images (maps pixel value --> quantile)
+    s_quantiles = np.cumsum(s_counts).astype(np.float64)
+    s_quantiles /= s_quantiles[-1]
+    t_quantiles = np.cumsum(t_counts).astype(np.float64)
+    t_quantiles /= t_quantiles[-1]
+
+    # interpolate linearly to find the pixel values in the template image
+    # that correspond most closely to the quantiles in the source image
+    interp_t_values = np.interp(s_quantiles, t_quantiles, t_values)
+
+    #Reshapes the corresponding values to the indexes and reshapes the array to input
+    final_image_data = interp_t_values[bin_idx].reshape(oldshape)
+    #final_image_data[indx] = 0
+
+    #Saves the output data
+    img = nib.Nifti1Image(final_image_data, patient.affine, patient.header)
+    nib.save(img, output_nii)
+
+    return output_nii
+
+
+def logpow_histogram_matching(reference_nii, input_nii, output_nii):
+
+    # Load the template image
+    template = nib.load(reference_nii)
+    nt_data = template.get_data()[:,:,:]
+    
+    # Load the patient image
+    patient = nib.load(input_nii)
+    pt_data = patient.get_data()[:,:,:]
+
+    # Stores the image data shape that will be used later
+    oldshape = pt_data.shape
+
+    # Converts the data arrays to single dimension and normalizes by the maximum
+    nt_data_array = nt_data.ravel()
+    pt_data_array = pt_data.ravel()
+
+    # get the set of unique pixel values and their corresponding indices and counts
+    s_values, bin_idx, s_counts = np.unique(pt_data_array, return_inverse=True, return_counts=True)
+    t_values, t_counts = np.unique(nt_data_array, return_counts=True)
+
+    s_counts = np.power(np.log10(s_counts + 1),2.5)
+    t_counts = np.power(np.log10(t_counts + 1),2.5)
+
+    # take the cumsum of the counts and normalize by the number of pixels to
+    # get the empirical cumulative distribution functions for the source and
+    # template images (maps pixel value --> quantile)
+    s_quantiles = np.cumsum(s_counts).astype(np.float64)
+    s_quantiles /= s_quantiles[-1]
+    t_quantiles = np.cumsum(t_counts).astype(np.float64)
+    t_quantiles /= t_quantiles[-1]
+
+    # interpolate linearly to find the pixel values in the template image
+    # that correspond most closely to the quantiles in the source image
+    interp_t_values = np.interp(s_quantiles, t_quantiles, t_values)
+
+    #Reshapes the corresponding values to the indexes and reshapes the array to input
+    final_image_data = interp_t_values[bin_idx].reshape(oldshape)
+    #final_image_data[indx] = 0
+
+    #Saves the output data
+    img = nib.Nifti1Image(final_image_data, patient.affine, patient.header)
+    nib.save(img, output_nii)
+
+    return output_nii
+
+
+def bi_histogram_matching(reference_nii, input_nii, output_nii):
+
+    # Load and prepare the template image
+    template = nib.load(reference_nii)
+    nt_data = template.get_data()[:,:,:]
+    indx = np.where(nt_data < 0)
+    # Set array values in the interval to val
+    nt_data[indx] = 0
+    nt_max = np.amax(nt_data)
+    nt_data = (255/nt_max)* nt_data
+
+    # Load and prepare the patient image
+    patient = nib.load(input_nii)
+    pt_data = patient.get_data()[:,:,:]
+    indx = np.where(pt_data < 0)
+    # Set array values in the interval to val
+    pt_data[indx] = 0
+    pt_max = np.amax(pt_data)
+    pt_data = (255/pt_max)* pt_data
+    oldshape = pt_data.shape
+    
+    # We adjust first the low part of the histogram
+    nt_data_low = nt_data
+    indx = np.where(nt_data > 128)
+    nt_data_low[indx] = 0
+    pt_data_low = pt_data
+    indx = np.where(pt_data > 128)
+    pt_data_low[indx] = 0
+    # We save data_low just for checking for the moment
+    img = nib.Nifti1Image(pt_data_low, patient.affine, patient.header)
+    nib.save(img, input_nii[0:-4] + '_low.nii')
+
+    # Converts the data arrays to single dimension and histograms the first part of the data
+    nt_data_array = nt_data_low.ravel()
+    pt_data_array = pt_data_low.ravel()
+    s_values, bin_idx, s_counts = np.unique(pt_data_array, return_inverse=True, return_counts=True)
+    t_values, t_counts = np.unique(nt_data_array, return_counts=True)
+    s_quantiles = np.cumsum(s_counts).astype(np.float64)
+    s_quantiles /= s_quantiles[-1]
+    t_quantiles = np.cumsum(t_counts).astype(np.float64)
+    t_quantiles /= t_quantiles[-1]
+    interp_t_values = np.interp(s_quantiles, t_quantiles, t_values)
+    final_image_data_low = interp_t_values[bin_idx].reshape(oldshape)
+    
+    # We prepare now to adjust the second part of the histogram
+    nt_data = template.get_data()[:,:,:]
+    indx = np.where(nt_data < 0)
+    nt_data[indx] = 0
+    nt_max = np.amax(nt_data)
+    nt_data = (255/nt_max)* nt_data
+
+    pt_data = patient.get_data()[:,:,:]
+    indx = np.where(pt_data < 0)
+    pt_data[indx] = 0
+    pt_max = np.amax(pt_data)
+    pt_data = (255/pt_max)* pt_data
+    oldshape = pt_data.shape
+
+    nt_data_high = nt_data
+    indx = np.where(nt_data <= 128)
+    nt_data_high[indx] = 0
+    indx = np.where(nt_data > 128)
+    nt_data_high[indx] = nt_data_high[indx]-128
+    pt_data_high = pt_data
+    indx = np.where(pt_data <= 128)
+    pt_data_high[indx] = 0
+    indx_pth = np.where(pt_data_high > 128)
+    pt_data_high[indx_pth] = pt_data_high[indx_pth]-128
+
+    # Converts the data arrays to single dimension and histograms the second part of the data
+    nt_data_array = nt_data_high.ravel()
+    pt_data_array = pt_data_high.ravel()
+    s_values, bin_idx, s_counts = np.unique(pt_data_array, return_inverse=True, return_counts=True)
+    t_values, t_counts = np.unique(nt_data_array, return_counts=True)
+    s_quantiles = np.cumsum(s_counts).astype(np.float64)
+    s_quantiles /= s_quantiles[-1]
+    t_quantiles = np.cumsum(t_counts).astype(np.float64)
+    t_quantiles /= t_quantiles[-1]
+    interp_t_values = np.interp(s_quantiles, t_quantiles, t_values)
+    final_image_data_high = interp_t_values[bin_idx].reshape(oldshape)
+    
+    final_image_data_high[indx_pth] = final_image_data_high[indx_pth]+128
+    final_image_data = final_image_data_high + final_image_data_low
+
+    #Saves the output data
+    img = nib.Nifti1Image(final_image_data, patient.affine, patient.header)
+    nib.save(img, output_nii)
+
+
+    return output_nii
+
+
+def exact_histogram_matching(reference_nii, input_nii, output_nii, number_kernels=3):
+
+    from histogram_matching import ExactHistogramMatcher
+    
+    template = nib.load(reference_nii)
+    nt_data = template.get_data()[:,:,:]
+    scaled_nt_data = np.round(1023 * (nt_data / np.max(nt_data)))
+    scaled_nt_data = np.asarray(scaled_nt_data, np.uint16)
+
+    patient = nib.load(input_nii)
+    pt_data = patient.get_data()[:,:,:]
+    scaled_pt_data = np.round(1023 * (pt_data / np.max(pt_data)))
+    scaled_pt_data = np.asarray(scaled_pt_data, np.uint16)
+
+    reference_histogram = ExactHistogramMatcher.get_histogram(scaled_nt_data, 10)
+    matched_img = ExactHistogramMatcher.match_image_to_histogram(pt_data, reference_histogram,number_kernels)
+
+    img = nib.Nifti1Image(matched_img, patient.affine, patient.header)
+    nib.save(img, output_nii)
+
+    return output_nii
+
---
+++ setup.py
@@ -0,0 +1,54 @@
+import os
+from os.path import join, exists, isdir, dirname
+from subprocess import getstatusoutput as getoutput
+
+def rsystem(command):
+    """
+    Executes command, raise an error if it fails and send status to logfile
+    :param command: command to be executed
+    :return: 
+    """
+    print("\nEXE:", command)
+    
+    if os.system(command) != 0:
+        print("Error executing:", command)
+        raise TypeError(command)
+    else:
+        print("Done executing:", command)
+
+def download_ehm():
+
+    source = 'https://github.com/txusser/ExactHistogramSpecification.git'
+
+    icom = 'git clone %s exact_hm'
+    rsystem(icom)
+
+def unpack_example():
+    
+
+def install_soap():
+    """
+    Execute installation of the required packages and libraries
+    :return: 
+    """
+    print("Insatalling SOAP")
+
+    icom = 'sudo apt update'
+    rsystem(icom)
+
+    # Install and upgrade PIP
+    icom = 'sudo apt -y install python-pip'
+    rsystem(icom)
+    icom = 'pip install --upgrade pip'
+    rsystem(icom)
+
+    # Install numpy
+    icom = 'sudo  pip install numpy'
+    rsystem(icom)
+
+    # Install Nibabel
+    icom = 'sudo pip install nibabel'
+    rsystem(icom)
+
+    icom = 'sudo pip install scipy'
+    rsystem(icom)
